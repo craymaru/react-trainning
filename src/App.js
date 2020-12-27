cade25c888502d2d6d4a4eea90e2f754118758d6
Cray <craymaru@gmail.com>
2020-12-27 16:18:21 +0900

    create list compornent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from 'react';
+import { List } from "./List";
 
+let count = 0
 function App() {
+  const [description, setDescription] = useState('Before click');
+  
+  const changeDescription = () => {
+    setDescription(`Click: ${++count}`)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List className="List" title="Languages List" />
+      <button onClick={changeDescription}>Click!</button>
+      <p>{description}</p>
+      {/* <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+      Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+      >
+      Learn React
+      </a>
+      
+    </header> */}
     </div>
   );
 }
