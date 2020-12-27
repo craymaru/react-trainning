298ae9af021341a4c3fcd016c7e22c3bfb143c35
Cray <craymaru@gmail.com>
2020-12-27 16:38:06 +0900

    delete commentout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useState } from 'react';
 import { List } from "./List";
 
 let count = 0
 function App() {
-  const [description, setDescription] = useState('Before click');
-  
+  const [description, setDescription] = useState(0);
+  console.log(description)
   const changeDescription = () => {
     setDescription(`Click: ${++count}`)
   }
@@ -16,21 +15,6 @@ function App() {
       <List className="List" title="Languages List" />
       <button onClick={changeDescription}>Click!</button>
       <p>{description}</p>
-      {/* <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-      Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-      >
-      Learn React
-      </a>
-      
-    </header> */}
     </div>
   );
 }
