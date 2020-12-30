01ddb3bb2ac6eba53a67f022b87caac74c67d5bb
Cray <craymaru@gmail.com>
2020-12-30 10:26:00 +0900

    delete useEffect in List

--- src/List.js
+++ src/List.js
@@ -1,14 +1,4 @@
-import { useEffect } from "react";
-
 export const List = ({ animals }) => {
-
-  useEffect(() => {
-    console.log("List: useEffect")
-    return () => {
-      console.log("List:useEffect:unmount")
-    }
-  })
-
   return (
     <div>
       <h4>Animals</h4>
